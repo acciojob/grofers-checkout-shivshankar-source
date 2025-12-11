a63d2287c05e712c8c026019d3fdff3d887434da
shivshankar-source <shivshakti.ss771@gmail.com>
2025-12-11 22:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ document.body.appendChild(getSumBtn);
 
 let allMoneyList = document.querySelectorAll(".price");
 let table = document.querySelector("table");
-
-const getSum = () => {
-  let sum = 0;
-  for (let t of allMoneyList) {
-    sum += parseFloat(t.innerText);
-  }
-
   let tr = document.createElement("tr");
   let td = document.createElement("td");
-	td.className="price"
+ let sum = 0;
+const getSum = () => {
+ for (let t of allMoneyList) {
+    sum += parseFloat(t.innerText);
+  }
+	
+  td.className="price"
   td.innerText = sum;  // keep it simple
   tr.append(td);
-  table.append(tr);
 };
-
 getSumBtn.addEventListener("click", getSum);
+
+  table.append(tr);
+
